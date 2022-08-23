8f21995a811357a63bb97725b66e2f663d33622d
johnNkwachi <jnkwachi@gmail.com>
2022-08-23 17:05:33 -0400

    comprehension

--- introductionToPython/set.py
+++ introductionToPython/set.py
@@ -5,4 +5,10 @@ s2 = {1,2,4}
 s3 = {1,3,5}
 print(s2.union(s3))
 
-print(s2&s3)
+# to check element that appears in the both
+# use intersection or &
+print(s2.intersection(s3))
+
+# to check the differences between both set
+# use difference or -
+print(s2.difference(s3))
